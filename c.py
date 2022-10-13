7e352851f1adedce75b03407aa6a08e0b84fb89d
suin <parksuin55@gmail.com>
2022-10-13 09:53:28 +0900

    [modify] 여러사람인식 for문으로 변경

--- c.py
+++ c.py
@@ -10,30 +10,8 @@ result = results.pandas().xyxy[0].to_numpy()
 result = [item for item in result if item[6]=='person']
 
 tmp_img = cv2.imread('people.jpg')
-
 print(tmp_img.shape)
-cropped = tmp_img[int(result[0][1]):int(result[0][3]), int(result[0][0]):int(result[0][2])]
-print(cropped.shape)
-cv2.imwrite('people1.png', cropped)
-
-print(tmp_img.shape)
-cropped = tmp_img[int(result[1][1]):int(result[1][3]), int(result[1][0]):int(result[1][2])]
-print(cropped.shape)
-cv2.imwrite('people2.png', cropped)
-
-print(tmp_img.shape)
-cropped = tmp_img[int(result[2][1]):int(result[2][3]), int(result[2][0]):int(result[2][2])]
-print(cropped.shape)
-cv2.imwrite('people3.png', cropped)
-
-print(tmp_img.shape)
-cropped = tmp_img[int(result[3][1]):int(result[3][3]), int(result[3][0]):int(result[3][2])]
-print(cropped.shape)
-cv2.imwrite('people4.png', cropped)
-
-print(tmp_img.shape)
-cropped = tmp_img[int(result[4][1]):int(result[4][3]), int(result[4][0]):int(result[4][2])]
-print(cropped.shape)
-cv2.imwrite('people5.png', cropped)
-# cv2.rectangle(tmp_img, (int(results.xyxy[0][0][0].item()), int(results.xyxy[0][0][1].item())), (int(results.xyxy[0][0][2].item()), int(results.xyxy[0][0][3].item())), (255,255,255))
-# cv2.imwrite('result.png', tmp_img)
+for i in range(len(result)):
+    cropped = tmp_img[int(result[i][1]):int(result[i][3]), int(result[i][0]):int(result[i][2])]
+    print(cropped.shape)
+    cv2.imwrite(f'people{i}.png', cropped)
